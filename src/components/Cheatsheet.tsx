6b9324820d2c1c9d1a514e76454c67904e93b9ac
Mayk Brito <maykbrito@gmail.com>
2021-10-04 14:48:25 -0300

    chore: eslint it!

--- src/components/Cheatsheet.tsx
+++ src/components/Cheatsheet.tsx
@@ -1,20 +1,20 @@
-import { Header } from "./Header";
-import { ISnippetElements } from "../models/Snippet";
+import { Header } from './Header'
+import { ISnippetElements } from '../models/Snippet'
 
 export type CheatsheetProps = {
-  title: string;
-  elements: ISnippetElements[];
-};
+  title: string
+  elements: ISnippetElements[]
+}
 
 export function Cheatsheet({ title, elements }: CheatsheetProps) {
   return (
     <>
       <Header title={title} />
-      {elements.map((element) => (
+      {elements.map(element => (
         <section id={element?.id} key={element?.id}>
           <h2>{element?.title}</h2>
           <ul>
-            {element?.lines.map((line) => (
+            {element?.lines.map(line => (
               <li key={line?.snippet}>
                 <span>{line?.snippet}</span>
                 {line?.description}
@@ -24,5 +24,5 @@ export function Cheatsheet({ title, elements }: CheatsheetProps) {
         </section>
       ))}
     </>
-  );
+  )
 }
